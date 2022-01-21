bb23e376fac240f82563d71a27b4e1c28781bed1
Sahitya77 <sahityanijhawan@gmail.com>
2022-01-21 15:18:51 +0530

    challenge-3 completed!

--- js/script.js
+++ js/script.js
@@ -19,14 +19,16 @@ function generatecat(){
     div.appendChild(image);
 }
 function rpsgame(yourChoice){
-    console.log(yourChoice);
     var humanchoice,botchoice;
-    //humanchoice=yourChoice.id;
+    humanchoice=yourChoice.id;
+    console.log(humanchoice);
     botchoice=numbertochoice(random());
-    alert(botchoice);
-    //results=decidewinner(humanchoice,botchoice);
-    //message=finalmessage(results);
-    //rpsfrontend(yourChoice.id,botchoice,message);
+    console.log(botchoice);
+    results=decidewinner(humanchoice,botchoice);
+    console.log(results);
+    message=finalmessage(results);
+    console.log(message);
+    rpsfrontend(yourChoice.id,botchoice,message);
 }
 
 function random(){
@@ -35,4 +37,53 @@ function random(){
 
 function numbertochoice(number){
     return ['rock','paper','scissors'][number];
+}
+function decidewinner(yourChoice,computerchoice){
+    var rpsdatabase ={
+        'rock':{
+            'scissors':1,'rock':0.5,'paper':0
+        },
+        'paper':{
+            'rock':1,'paper':0.5,'scissors':0
+        },
+        'scissors':{
+            'paper':1,'scissors':0.5,'rock':0
+        }
+    };
+    var yourscore=rpsdatabase[yourChoice][computerchoice];
+    var computerscore=rpsdatabase[computerchoice][yourChoice];
+    return [yourscore,computerscore];
+}
+function finalmessage(results){
+    if(results[0]===0){
+        return {'message':'You Lost!','color':'red'};
+    }
+    else if(results[0]===0.5){
+        return {'message':'You Tied!','color':'yellow'};
+    }
+    else if(results[0]===1){
+        return {'message':'You Won!','color':'green'};
+    }
+}
+function rpsfrontend(humanimagechoice,botimagechoice,finalmessage){
+    var imagesdatabase={
+        'rock':document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissors':document.getElementById('scissors').src
+    };
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humandiv=document.createElement('div');
+    var botdiv=document.createElement('div');
+    var messagediv=document.createElement('div');
+
+    humandiv.innerHTML="<img src='"+ imagesdatabase[humanimagechoice] + "'height =150 width =150 style='box-shadow:0px 10px 50px rgba(37,50,233,1);'>"
+    botdiv.innerHTML="<img src='"+ imagesdatabase[botimagechoice] + "'height =150 width =150 style='box-shadow:0px 10px 50px rgba(243,38,24,1);'>"
+    messagediv.innerHTML="<h1 style='color: "+finalmessage['color']+";font-size:60px; padding:30px;'>"+finalmessage['message']+"</h1>"
+    document.getElementById('flex-box-rps-div').appendChild(humandiv);
+    document.getElementById('flex-box-rps-div').appendChild(messagediv);
+    document.getElementById('flex-box-rps-div').appendChild(botdiv);
+    
 }
