93b821c39d0e4f05a4b96f617b96b0ce1a7bf716
Sahitya77 <sahityanijhawan@gmail.com>
2022-01-26 21:52:53 +0530

    challenge-5 changes css!

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function ageindays(){
     var birthyear=prompt("What year were you born?");
     var age=(2022-birthyear)*365;
     var h1=document.createElement('h1');
-    var text=document.createTextNode('You are'+age+' days old.');
+    var text=document.createTextNode('You are '+age+' days old.');
     h1.setAttribute('id','ageindays');
     h1.appendChild(text);
     document.getElementById('flex-box-result').appendChild(h1);
